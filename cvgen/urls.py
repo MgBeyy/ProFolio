09699fce10c880e9b09df4acfb38159251de1021
zaid <zaidalmoughrabi@gmail.com>
2025-04-27 18:30:58 +0300

    Created a test end point

--- cvgen/urls.py
+++ cvgen/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from cvgen import views
 
 urlpatterns = [
-    path('', views.ProfileFromFormAPIViews.as_view(), name='profile_from_form'),
+    path('profile/', views.ProfileFromFormAPIViews.as_view(), name='profile_from_form'), # Todo: Change 'profile/' for more readability. 
+    
+    
+    path('test/', views.TestApiViews.as_view(), name='test'), # Todo: Delete this before deploying
 ]
 
--- cvgen/views.py
+++ cvgen/views.py
@@ -1,4 +1,7 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 # Todo: ProfileViews from CV doc (Get, Post, Put, Delete)
@@ -20,3 +23,18 @@ class ProfileFromFormAPIViews(APIView):
     def delete():
         pass
     
+
+
+class TestApiViews(APIView):
+    
+    def get(self, request):
+        return Response(status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response(status=status.HTTP_201_CREATED)
+
+    def put(self, request):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        return Response(status=status.HTTP_204_NO_CONTENT)
