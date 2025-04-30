3f5da157abddaadc40676640796a504eda7c16fc
zaid <zaidalmoughrabi@gmail.com>
2025-04-30 10:10:26 +0300

    fix - fixed login process

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", views.RegisterApiView.as_view(), name="register"),
-    path("token/", TokenObtainPairView.as_view(), name="token"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
 ]
