ea5a9d542cc2ec705a475b5361360268a69303ef
zaid <zaidalmoughrabi@gmail.com>
2025-05-08 23:42:04 +0300

    feat - added .env file to keep keys secure

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# ! SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r6)y@5q*e15k_#12o@+w_5v4tzbu0+07sg6d^y0e73*ov+f!5v'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
-# ! SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
 # Application definition
