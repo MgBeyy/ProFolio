173b00fd65836288a900d9d960573a9c2a6b3b3e
zaid <zaidalmoughrabi@gmail.com>
2025-05-16 10:39:59 +0300

    fix - refactored VerifyEmailApiView to improve user token handling and updated response messages for clarity

--- accounts/views.py
+++ accounts/views.py
@@ -104,12 +104,12 @@ class VerifyEmailApiView(APIView):
             )
         user = User.objects.filter(email=user_email).first()
 
-        user_token = UserEmailToken.objects.get_or_create(
-            user=user,
-            email_verification_token=get_random_string(40),
-            email_verification_expire=timezone.now() + timedelta(minutes=10),
-            is_verified=False,
-        )
+        user_token, _ = UserEmailToken.objects.get_or_create(user=user)
+        user_token.email_verification_token = get_random_string(40)
+        user_token.email_verification_expire = timezone.now() + timedelta(minutes=10)
+        user_token.is_verified = False
+
+        user_token.save()
 
         host = get_current_host_url(request)
 
@@ -124,9 +124,9 @@ class VerifyEmailApiView(APIView):
             return Response(
                 {
                     "result": "success",
-                    "message": "Successfully created account. Please check your email inbox to verify the email.",
+                    "message": "Successfully sent verification email. Please check your email inbox to verify the email.",
                 },
-                status=status.HTTP_201_CREATED,
+                status=status.HTTP_200_OK,
             )
         else:
             return Response(
@@ -205,7 +205,7 @@ class UserApiView(APIView):
         return Response(
             {
                 "result": "success",
-                "message": "Successfly updated user data.",
+                "message": "successfully updated user data.",
             },
             status=status.HTTP_200_OK,
         )
@@ -228,6 +228,7 @@ class UserApiView(APIView):
 class VerifyEmailConfirmApiView(APIView):
     def get(self, request):
         token = request.query_params.get("token")
+        print("TOKEN", token)
 
         if not UserEmailToken.objects.filter(email_verification_token=token).exists():
             return Response(
