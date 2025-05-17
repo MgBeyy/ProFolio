85b4fae229f872f94e0c4166b143b0d2f8ed4283
zaid <zaidalmoughrabi@gmail.com>
2025-05-18 01:44:15 +0300

    fix - fixed prompt

--- helpers/ai_prompts.py
+++ helpers/ai_prompts.py
@@ -1,7 +1,7 @@
 ANALYZE_CV_PROMPT = """You are given a resume below. Analyze it and return the data in the following structured JSON format. Do not include any explanation or extra commentary. Return only valid JSON. The json tags must be in English and match the template. The information in it must match the language of your CV. Dates must be in YYYY-MM-DD format. If there is no day, you can type 01. However, if there is no year or month, it returns an empty string.
 
 {
-  "summary": "A short general summary (if there is no summary in the pdf file, write one).",
+  "summary": "A short general summary (if there is no summary in the pdf file, write one in the language of CV).",
   "experience": [
     {
       "company": "Company name",
@@ -15,6 +15,7 @@ ANALYZE_CV_PROMPT = """You are given a resume below. Analyze it and return the d
     {
       "school": "School name",
       "degree": "Degree or major",
+      "department": "Department name",
       "start_date": "YYYY-MM-DD",
       "end_date": "YYYY-MM-DD",
       "description": "Optional details about education"
