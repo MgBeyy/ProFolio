bab3c109aef5a637b71b8d177259565d8c4d3a8a
zaid <zaidalmoughrabi@gmail.com>
2025-05-18 01:41:09 +0300

    fix - fixed models and made unneeded fields nullable

--- cvgen/models.py
+++ cvgen/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,7 +10,6 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-
 
 
 class Profile(BaseModel):
@@ -20,11 +21,13 @@ class Profile(BaseModel):
 
 
 class Experience(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="experience")
-    company = models.CharField(max_length=100)
-    position = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="experience"
+    )
+    company = models.CharField(max_length=100, blank=True, null=True)
+    position = models.CharField(max_length=100, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -32,11 +35,14 @@ class Experience(BaseModel):
 
 
 class Education(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="education")
-    school = models.CharField(max_length=100)
-    degree = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="education"
+    )
+    school = models.CharField(max_length=100, blank=True, null=True)
+    degree = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -44,11 +50,13 @@ class Education(BaseModel):
 
 
 class Certification(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="certifications")
-    name = models.CharField(max_length=100)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="certifications"
+    )
+    name = models.CharField(max_length=100, blank=True, null=True)
     organization = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -56,27 +64,32 @@ class Certification(BaseModel):
 
 
 class Language(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="languages")
-    language = models.CharField(max_length=100)
-    level = models.CharField(max_length=100)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="languages"
+    )
+    language = models.CharField(max_length=100, blank=True, null=True)
+    level = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.language} ({self.level})"
 
 
-
 class Skill(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="skills")
-    name = models.CharField(max_length=100)
-    level = models.CharField(max_length=100)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="skills"
+    )
+    name = models.CharField(max_length=100, blank=True, null=True)
+    level = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.level})"
 
 
 class Project(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="projects")
-    title = models.CharField(max_length=255)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="projects"
+    )
+    title = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     technologies = models.TextField(blank=True, null=True)
     project_url = models.TextField(blank=True, null=True)
@@ -85,9 +98,10 @@ class Project(BaseModel):
         return self.title
 
 
-
 class Interview(BaseModel):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="interview")
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="interview"
+    )
     results = models.IntegerField(blank=True, null=True)
     feedback = models.TextField(blank=True, null=True)
 
@@ -100,23 +114,27 @@ class InterviewQuestion(BaseModel):
     user_answer = models.TextField(blank=True, null=True)
     correct_answer = models.TextField(blank=True, null=True)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name="questions")
-    interview = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="questions")
+    interview = models.ForeignKey(
+        Interview, on_delete=models.CASCADE, related_name="questions"
+    )
 
     def __str__(self):
         return f"Question about {self.skill.name}"
 
 
-
 class Cv(BaseModel):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="cv")
-    version_name = models.CharField(max_length=100, blank=True, null= True,)
-    file = models.FileField(upload_to='cv/')
-    content = models.TextField(blank=True, null= True)
+    version_name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    file = models.FileField(upload_to="cv/")
+    content = models.TextField(blank=True, null=True)
     is_ai_generated = models.BooleanField(blank=True, null=False)
 
     def __str__(self):
         return f"{self.profile.user.username}'s CV - {self.version_name or 'Default'}"
-
 
 
 # ?: Should we add Cover Letters ??
