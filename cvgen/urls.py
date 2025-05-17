6551a4949a5b50b92a7e6d8a630184e935a3d013
zaid <zaidalmoughrabi@gmail.com>
2025-05-18 01:44:36 +0300

    feat - added routers for each cv part

--- cvgen/urls.py
+++ cvgen/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from cvgen import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register("profile", views.ProfileViewSet, basename="profile")
+router.register("experience", views.ExperienceViewSet, basename="experience")
+router.register("education", views.EducationViewSet, basename="education")
+router.register("certification", views.CertificationViewSet, basename="certification")
+router.register("language", views.LanguageViewSet, basename="language")
+router.register("skill", views.SkillViewSet, basename="skill")
+router.register("project", views.ProjectViewSet, basename="project")
+
 
 
 urlpatterns = [
@@ -9,6 +22,15 @@ urlpatterns = [
     path('analyze_cv/', views.AnalyzeCvWithAiApiView.as_view(), name='analyze_cv'),
     
     path('test/', views.TestApiViews.as_view(), name='test'), # Todo: Delete this before deploying
+
+
+    path('', include(router.urls)),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
 
